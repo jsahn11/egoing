88e2d0135531565e2473c5080c41bba583c30d7f
jsahn11 <jsahn1224@gmail.com>
2018-05-29 13:21:09 +0900

    6

--- webserver.js
+++ webserver.js
@@ -1,11 +1,14 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 1337;
+const port = 3000;
 
-http.createServer((req, res) => {
-res.writeHead(200, { 'Content-Type': 'text/plain' });
-res.end('Hello World\n');
-}).listen(port, () => {
-console.log(`Server running at http://${hostname}:${port}/`);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });

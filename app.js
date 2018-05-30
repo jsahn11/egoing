9116708ea8f5e6016909266f16be2ec7ee436164
jsahn11 <jsahn1224@gmail.com>
2018-05-30 17:47:13 +0900

    0530company

--- app.js
+++ app.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post('/profile', upload.array(), function (req, res, next) {
+  console.log(req.body);
+  res.json(req.body);
+});
 app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
+app.get('/form', function(req, res){
+  res.render('form');
+});
+app.get('/form_receiver', function(req, res){
+    var title = req.query.title;
+    var description = req.query.description;
+    res.send(title+','+description);
+});
+app.post('/form_receiver', function(req, res){
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send(title+','+description);
+});
+app.get('/topic/:id', function(req, res){
+  var topics = [
+    'Javascript is....',
+    'Nodejs is...',
+    'Express is...'
+  ];
+  var output = `
+  <a href="/topic?id=0">JavaScript</a><br>
+  <a href="/topic?id=1">Nodejs</a><br>
+  <a href="/topic?id=2">Express</a><br><br>
+  ${topics[req.params.id]}
+  `
+  res.send(output);
+});
+app.get('/topic/:id/:mode', function(req, res){
+  res.send(req.params.id+','+req.params.mode)
+});
 app.get('/template', function(req, res){
   res.render('temp', {time:Date(), title:'Jade'});
 })
@@ -35,7 +76,7 @@ app.get('/dynamic', function(req, res){
 });
 app.get('/route', function(req, res){
     res.send('Hello Router, <img src="/bts.jpg">')
-})
+});
 app.get('/login', function(req, res){
     res.send('<h1>Login please</h1>');
 });
